5d7c87c95e5f7509bfd637fd223865b23f12b93c
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-06-09 10:33:52 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # S3 download and delete
 A simple python script that downloads and deletes files from an S3 bucket
+
+## Usage
+
+```
+AWS_ACCESS_KEY_ID=KEY AWS_SECRET_ACCESS_KEY=SECRET python go.py --bucket=BUCKET --directory=RELATIVE_PATH
+```
--- go.py
+++ go.py
@@ -1,47 +1,34 @@
 """
 s3-download-and-delete
 
-Usage:
-  python go.py --bucket foo --directory .
+Usage: go.py --bucket=<bucket> --directory=<directory>
 
 Options:
- --bucket           The name of the S3 bucket to download stuff off and clear out
- --directory        The directory to download the files to
-
-Examples:
-  python go.py --bucket foo --directory .
-
-Help:
-  Visit https://github.com/wearescouting/s3-download-and-delete
+ --bucket=<bucket>              The name of the S3 bucket to download stuff off and clear out
+ --directory=<directory>        The directory to download the files to
 """
 
 import boto3
-import docopt
+from docopt import docopt
 import os
-import sys
 
 client = boto3.client('s3')
 
-def get_contents():
+def get_contents(bucket, directory):
   contents = []
   paginator = client.get_paginator('list_objects_v2')
-  page_iterator = paginator.paginate(Bucket=os.getenv('BUCKET'))
+  page_iterator = paginator.paginate(Bucket=bucket)
 
   for page in page_iterator:
     contents = contents + page['Contents']
 
   for content in contents:
-    client.download_file(os.getenv('BUCKET'), content['Key'], os.getenv('DIRECTORY') + '/' + content['Key'])
+    client.download_file(bucket, content['Key'], os.path.join(os.getcwd(), directory, content['Key']))
 
-  client.delete_objects(Bucket=os.getenv('BUCKET'), Delete={
+  client.delete_objects(Bucket=bucket, Delete={
     'Objects': map(lambda x: {'Key': x['Key']}, contents)
   })
 
-get_contents()
-# def main():
-#   options = docopt(__doc__, version='1.0.0')
-
-#   for k, v in options.iteritems():
-
-
-# main()
+if __name__ == '__main__':
+  options = docopt(__doc__)
+  get_contents(options['--bucket'], options['--directory'])
